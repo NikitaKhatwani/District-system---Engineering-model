bc96fe789dcc6b2828a792e28f7f256dfff2f54c
Nikita Khatwani <nikitakhatwani07@gmail.com>
2024-04-25 16:35:00 -0400

    v5

--- ucsbdistrict/CUP.py
+++ ucsbdistrict/CUP.py
@@ -31,14 +31,8 @@ class CUP(BaseModel):
     
 
     def is_on(self,tank_schedule,month,hour):
-
-        
-
-        # retrieve the value from the scheduling DataFrame based on month and hour
+        #loc is used to get the valu at the month and hour of the schedule using "labels- row or col name"
         value = tank_schedule.loc[tank_schedule["Month"]== month,hour]
-        # print("vv",value,month,hour)
-        # return value.iloc[0]
-        #check if value exists and is equal to 1
         if  value.iloc[0]==1:
             return True
         else:
@@ -49,17 +43,6 @@ class CUP(BaseModel):
     @property    
     def HW_charging(self):
         df = pd.DataFrame()
-        # print(self.timeStamp['Date and Time'])
-        # # month = self.timeStamp['Date and Time'].dt.month
-        # # hour = self.timeStamp['Date and Time'].dt.hour
-        # # print("month", month)
-        # # print("hour", hour)
-        # print("Htank",self.hotTank_schedule["Month"])
-        # print("Htank",self.hotTank_schedule.loc[self.hotTank_schedule["Month"]== 2,15])
-        # print("Htank",self.hotTank_schedule[self.hotTank_schedule["Month"]== 2])
-        # df["shiftCharging"] = self.timeStamp.apply(lambda ts: self.is_on(ts,self.hotTank_schedule,month,hour))
-
-        # df["shiftCharging"] = []  # Create an empty list to store results
         result = []
         for ts in self.timeStamp['Date and Time']:
             month = ts.month
@@ -81,7 +64,7 @@ class CUP(BaseModel):
 
             result.append(self.is_on(self.coldTank_schedule, month, hour))
         # print(df)
-        return result
+        return pd.Series(result)
     
 
 
@@ -102,8 +85,7 @@ class CUP(BaseModel):
 
         #initiate empty series to store results
         result_series = pd.Series(index=shiftCount.index)
-        print(type(df["shiftCount"]))
-        print(df["shiftCount"])
+
         for value in shiftCount.unique():
             
             #filter to every shiftcount value
@@ -162,11 +144,52 @@ class CUP(BaseModel):
         x= self.predictedDayLoad_shift(self.CHW_charging,self.shiftCount,self.totalCooling_load)
         return x
 
+
+    #checks total hours the tank is on
+    def total_on(self,tank_schedule,month):
+        #loc is used to get the valu at the month and hour of the schedule using "labels- row or col name"
+        value = tank_schedule.loc[tank_schedule["Month"]== month,"Total"]
+        return value.iloc[0]
+
+    @computed_field(return_type=MySeries)
+    @property    
+    def HL_totalShiftHours(self):
+
+        result = []
+        for ts in self.timeStamp['Date and Time']:
+            month = ts.month
+
+            result.append(self.total_on(self.hotTank_schedule, month))
+        # print(df)
+        return result
+    
+
+
+
+
+
+
+    @computed_field(return_type=MySeries)
+    @property    
+    def CL_totalShiftHours(self):
+
+        result = []
+        for ts in self.timeStamp['Date and Time']:
+            month = ts.month
+
+            result.append(self.total_on(self.coldTank_schedule, month))
+        # print(df)
+        return result
+    
+
+
     def compute_CUP(self):
         df=pd.DataFrame({
             "Hot Load Shift Charging": self.HW_charging,
             "Cold Load Shift Charging": self.CHW_charging,
-            "Hot Load Shift Count": self.shiftCount,
+            "Hot Load Shift Hours":  self.HL_totalShiftHours,
+            "Cold Load Shift Hours": self.CL_totalShiftHours,
+            "Shift Count": self.shiftCount,
             "Predicted Day's Heating Load (Btu/h)": self.predictedDay_heating,
             "Predicted Day's Cooling Load (Btu/h)": self.predictedDay_cooling,
             "Predicted Heating Load in Load Shift Window (Btu/h)":self.predictedDay_heating_shift,
--- ucsbdistrict/TES.py
+++ ucsbdistrict/TES.py
@@ -7,21 +7,27 @@ from ucsbdistrict.CUP import CUP
 class TES(BaseModel):
     # coolingLoad: pd.Series
     # heatingLoad : pd.Series
-    HW_upperTankVol : float
-    HW_lowerTankVol : float
-    HW_TankVol : float
-    HW_wallLoss : float
-    HW_ThermocloneLoss : float
+    HW_upperTankVol : float 
+    HW_lowerTankVol : float 
+    HW_TankVol : float 
+    HW_wallLoss : float 
+    HW_ThermocloneLoss : float 
     CHW_upperTankVol : float
-    CHW_lowerTankVol : float
-    CHW_TankVol : float
-    CHW_wallLoss : float
-    CHW_ThermocloneLoss : float
-    CUP_instance : CUP = None  # Default value None for address
-    HP_heatingCapacity: float
-    thermoclineDischarge: float
-    stopCharging: float
-    totalHeating_load : pd.Series
+    CHW_lowerTankVol : float 
+    CHW_TankVol : float 
+    CHW_wallLoss : float 
+    CHW_ThermocloneLoss : float 
+    CUP_instance : CUP = None  # Default value None for Cup instance
+    HP_heatingCapacity: float 
+    thermoclineDischarge: float 
+    stopCharging: float 
+    totalHeating_load : pd.Series 
+    HW_districtSTP : pd.Series 
+    districtHWRT : pd.Series 
+    hotMaxflow: float
+    coldMaxflow: float
+    CUP_output_df : pd.DataFrame =None
+    HW_upperTankTemp: float 
     # hotLoadShiftCharging : CUP.HW_charging
 
     class MySeries(BaseModel):
@@ -120,9 +126,13 @@ class TES(BaseModel):
 
 
 
+
+
+
+
     def compute(self):
         df = pd.DataFrame({
-            "check": self.hotCapacityHr,
+            "TES Hot Capacity hr-1 (%)": self.hotCapacityHr,
             "TES Hot Th after losses (Gal)_supply temp": self.hotThafterLoss_supply,
             "TES Hot Tc after losses (Gal)_return temp":self.hotThafterLoss_return,
             "TES Hot Thermocline (Gal)": self.hotThermocline,
@@ -133,30 +143,143 @@ class TES(BaseModel):
         })
         return df
     
+
+    # @computed_field(return_type=MySeries)   
+    # @property
+    # def CUP_output_df(self):
+    #     result_series = self.CUP_instance.compute_CUP()
+    #     print("result_series",result_series)
+    #     return result_series
+
+
+
+
     @computed_field(return_type=MySeries)   
     @property
     def hotStatus(self):
-        CUP_output_df = self.CUP_instance.compute_CUP()
-        
+        # CUP_output_df = self.CUP_instance.compute_CUP()
+        # print("CUP_output_df",CUP_output_df)
         # Apply conditions using pandas logical operations without direct if statements
-        condition_hold = (CUP_output_df["Hot Load Shift Charging"] & ((self.hotThafterLoss_supply + self.hotThermocline) * self.thermoclineDischarge >= self.HW_TankVol * self.stopCharging) | (self.totalHeating_load > self.HP_heatingCapacity))
-        condition_charge = CUP_output_df["Hot Load Shift Charging"] & ~condition_hold
+        condition_hold = (self.CUP_output_df["Hot Load Shift Charging"] & ((self.hotThafterLoss_supply + self.hotThermocline) * self.thermoclineDischarge >= self.HW_TankVol * self.stopCharging) | (self.totalHeating_load > self.HP_heatingCapacity))
+        condition_charge = self.CUP_output_df["Hot Load Shift Charging"] & ~condition_hold
         
         # Use numpy.select to assign results based on conditions
-        result_series = pd.Series(pd.NA, index=CUP_output_df.index)  # Initialize result series with NA
+        result_series = pd.Series(pd.NA, index=self.CUP_output_df.index)  # Initialize result series with NA
         result_series[condition_hold] = "Hold"
         result_series[condition_charge] = "Charge"
-        result_series[~CUP_output_df["Hot Load Shift Charging"]] = "Discharge"
+        result_series[~self.CUP_output_df["Hot Load Shift Charging"]] = "Discharge"
+        
+        return result_series
+        # return self.CUP_output_df["Hot Load Shift Charging"]
+
+
+    @computed_field(return_type=MySeries)   
+    @property
+    def hotLoadShiftChargeRate(self):
+        mask_OG =  (self.hotStatus == "Discharge") | (self.hotStatus == "Hold")
+
+        HW_STP = (self.HW_districtSTP-self.districtHWRT)
+        value1 = (self.HP_heatingCapacity-self.totalHeating_load)/500/HW_STP
+
+        ##value2
+        predictedHeatingMinusShift = self.CUP_output_df["Predicted Day's Heating Load (Btu/h)"]-self.CUP_output_df["Predicted Heating Load in Load Shift Window (Btu/h)"]
+        mask = self.CUP_output_df["Hot Load Shift Charging"] !=0
+        #initiate empty series with same index
+        value2 = pd.Series(0, index=self.CUP_output_df.index)
+        # #apply condition when mask is TRue(not zero)
+        # value2[mask] =  predictedHeatingMinusShift/500/HW_STP/self.CUP_output_df["Hot Load Shift Hours"]
+
+        # Compute the value for value2 and ensure dtype compatibility
+        computed_value = predictedHeatingMinusShift/500/HW_STP/self.CUP_output_df["Hot Load Shift Hours"]
+        value2[mask] = computed_value.astype(int)  # Convert to int64 dtype
+
+
+        ##value 3
+        value3= self.hotThermocline+self.hotThafterLoss_return*self.thermoclineDischarge
+
+        ##value 4
+        value4 = self.hotMaxflow
+        print("v1",value1)
+        print("v2",value2)
+        print("v3",value3)
+      
+ 
+        result_series = pd.Series([0]*8760)
+        result_series[mask_OG]=0
+        # Filter and compute minimum across valid rows
+        # valid_values = [value[~mask_OG] for value in [value1, value2, value3, value4]]
+        # result_series[~mask_OG] = np.minimum.reduce(valid_values)
+
+        valid_values = [value1, value2, value3, value4]
+        valid_series = [value[~mask_OG] for value in valid_values if isinstance(value, pd.Series)]
+        
+        if valid_series:
+            min_values = np.minimum.reduce(valid_series)
+            # Ensure `min_values` has a compatible dtype with `result_series`
+            min_values = min_values.astype(result_series.dtype)
+            result_series[~mask_OG] = min_values
+        
         
         return result_series
 
 
 
+    @computed_field(return_type=MySeries)   
+    @property
+    def H_flowIntoTES(self):
+        return np.where(self.hotStatus== "Charge", self.hotLoadShiftChargeRate,0)
+
+            
+    @computed_field(return_type=MySeries)   
+    @property
+    def H_tempTES(self):
+        result_series = pd.Series([self.HW_upperTankTemp]*8760)
+        return result_series
+    
+    @computed_field(return_type=MySeries)   
+    @property
+    def TES_H_flow(self):
+        return self.totalHeating_load/500/(self.H_tempTES-self.districtHWRT).astype(float)
+
+
+
+
+    @computed_field(return_type=MySeries)   
+    @property
+    def H_flowOutTES(self):
+        minimum = np.minimum(self.flowTES,self.hotMaxflow,(self.hotThafterLoss_supply/60).astype(float)).astype(float)
+        return np.where(self.hotStatus =="Discharge",minimum,0).astype(float)
+
+
+
+    @computed_field(return_type=MySeries)   
+    @property
+    def TES_H_tc(self):
+        value1 = self.hotThafterLoss_supply+self.H_flowIntoTES*60
+        value2 = np.where(self.H_flowOutTES*60>self.hotThafterLoss_supply,self.hotThafterLoss_supply,self.H_flowOutTES*60)
+        return value1 - value2
+
+
+    @computed_field(return_type=MySeries)   
+    @property
+    def TES_H_capacity(self):
+        try:
+            result_series = self.TES_H_tc/self.HW_TankVol
+        except ValueError:
+            result_series = 0
+        return result_series
 
 
     def compute_2(self):
         # return self.hotStatus
         df2 = pd.DataFrame({
-            "TES Hot Status": self.hotStatus
+            "TES Hot Status": self.hotStatus,
+            "Hot Load-Shift Charge Rate (gpm)":self.hotLoadShiftChargeRate,
+            "Flow into TES Hot Th (gpm)":self.H_flowIntoTES,
+            "TES Hot Th Previous Hour (°F)":self.H_tempTES,
+            "TES Flow to meet Campus Heating (gpm)":self.TES_H_flow,
+            "Flow out of TES Hot Th (gpm)" : self.H_flowOutTES,
+            "TES Hot Th (Gal)": self.TES_H_tc,
+            "TES_H_tc" : self.TES_H_capacity
         })                
         return df2
